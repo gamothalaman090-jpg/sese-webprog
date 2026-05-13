9230b3b34fbea6423a1d8c06425759867905fdf5
Kohaku <gamothalaman090@gmail.com>
2026-05-14 00:07:56 +0800

    production

--- sese-server/controllers/userController.js
+++ sese-server/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcryptjs'); // For password hashing
 const jwt = require('jsonwebtoken'); // For generating tokens
 
